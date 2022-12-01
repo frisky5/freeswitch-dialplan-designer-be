d06c6db41cc70e37a69fb4cdd9c186f98128bc34
frisky5 <mahmood.alkhalil@outlook.com>
2022-12-01 13:59:56 +0400

    initial commit

---
+++ src/main/java/org/tsuki/freeswitchdialplandesignerbe/FreeswitchDialplanDesignerBeApplication.java
@@ -0,0 +1,13 @@
+package org.tsuki.freeswitchdialplandesignerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeswitchDialplanDesignerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeswitchDialplanDesignerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tsuki/freeswitchdialplandesignerbe/FreeswitchDialplanDesignerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tsuki.freeswitchdialplandesignerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeswitchDialplanDesignerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
